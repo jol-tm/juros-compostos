0a9f25d24e0253ee375d87baf043bb93cf6e9481
João Lucas Teixeira Magalhães <163040452+jol-tm@users.noreply.github.com>
2024-09-30 17:13:01 -0300

    Código base

---
+++ script.js
@@ -0,0 +1,30 @@
+let capIni = document.getElementById("capitalInicial");
+let capIniDis = document.getElementById("capIniDis");
+let juros = document.getElementById("juros");
+let jurosDis = document.getElementById("jurosDis");
+let tempo = document.getElementById("tempo");
+let tempoDis = document.getElementById("tempoDis");
+let res = document.getElementById("resultado");
+
+capIni.addEventListener("input", () => {
+    getValues();
+})
+juros.addEventListener("input", () => {
+    getValues();
+})
+tempo.addEventListener("input", () => {
+    getValues();
+})
+
+function getValues(){
+    capIniDis.innerText = capIni.value;
+    jurosDis.innerText = juros.value;
+    tempoDis.innerText = tempo.value;
+    calc();
+
+}
+
+function calc(){
+    let montante = (capIni.value * ((1 + juros.value / 100) ** tempo.value)).toFixed(2).replaceAll(".", ",");
+    res.innerText = `R$${montante}`;
+}
